d56bc0a7e2bd58ebe72446f503f21e8c8dd6d441
王闪闪 <wangshanshan@wangshanshandemacbook-pro.local>
2016-04-14 16:54:15 +0800

    Initial Commit

---
+++ FirstKuangJia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstKuangJia
+//
+//  Created by 王闪闪 on 16/4/14.
+//  Copyright © 2016年 shanshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstKuangJia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstKuangJia
+//
+//  Created by 王闪闪 on 16/4/14.
+//  Copyright © 2016年 shanshan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
